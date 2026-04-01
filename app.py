6cb7a76574769de5d3bdb6b4406a928b0876562c
selensknmz-creator <selensknmz@gmail.com>
2026-04-01 21:28:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from webApp.flask_srvr import run_flask
 from managers.thread_manager import start_thread
 import time
 
-run_thread(run_flask, name="FlaskServer")
+start_thread(run_flask, name="FlaskServer")
 
 logging.info(config.id)
 
